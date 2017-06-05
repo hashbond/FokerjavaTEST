0e5e38161d57ef6be4519e44afd94251c15b5470
HASH <HASH@DESKTOP-N2OUCFD>
2017-06-05 11:31:39 +0530

    Game home page created

--- fxml/src/fxml/FXMLDocumentController.java
+++ fxml/src/fxml/FXMLDocumentController.java
@@ -18,6 +18,7 @@ import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.animation.TranslateTransition;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
 /**
@@ -38,44 +39,94 @@ public class FXMLDocumentController implements Initializable {
     private ImageView img1;//ace of spades
     @FXML
     private ImageView img4;//queen
+    @FXML
+    private Label F;
+    @FXML
+    private AnchorPane o;
+    @FXML
+    private Label F1;
+    @FXML
+    private Label k;
+    @FXML
+    private Label e;
+    @FXML
+    private Label r;
     
     //private 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        Circle circle = new Circle(100);
+       
         
         TranslateTransition transition = new TranslateTransition();
         transition.setDuration(Duration.seconds(2));
         transition.setNode(img);
-        
         transition.setToX(-350);
         
         TranslateTransition transition1 = new TranslateTransition();
         transition1.setDuration(Duration.seconds(2));
         transition1.setNode(img2);
-        
         transition1.setToX(-250);
 
         TranslateTransition transition2 = new TranslateTransition();
         transition2.setDuration(Duration.seconds(2));
         transition2.setNode(img4);
-        
         transition2.setToX(-50);
         
         TranslateTransition transition3 = new TranslateTransition();
         transition3.setDuration(Duration.seconds(2));
         transition3.setNode(img1);
-        
         transition3.setToX(-150);
-
-
-        //transition.setAutoReverse(true);
-       // transition.setCycleCount(TranslateTransition.INDEFINITE);
+        
+        TranslateTransition transition4 = new TranslateTransition();
+        transition4.setDuration(Duration.seconds(6));
+        transition4.setNode(F);
+        transition4.setToY(-5);
+        transition4.setAutoReverse(true);
+        transition4.setCycleCount(TranslateTransition.INDEFINITE);
+        
+       
+        
+        
+        
+        TranslateTransition transition5 = new TranslateTransition();
+        transition5.setDuration(Duration.seconds(6));
+        transition5.setNode(e);
+        transition5.setToY(-10);
+        transition5.setAutoReverse(true);
+        transition5.setCycleCount(TranslateTransition.INDEFINITE);
+        
+        TranslateTransition transition6 = new TranslateTransition();
+        transition6.setDuration(Duration.seconds(6));
+        transition6.setNode(F1);
+        transition6.setToY(5);
+        transition6.setAutoReverse(true);
+        transition6.setCycleCount(TranslateTransition.INDEFINITE);
+        
+          TranslateTransition transition7 = new TranslateTransition();
+        transition7.setDuration(Duration.seconds(6));
+        transition7.setNode(k);
+        transition7.setToY(-5);
+        transition7.setAutoReverse(true);
+        transition7.setCycleCount(TranslateTransition.INDEFINITE);
+        
+          TranslateTransition transition8 = new TranslateTransition();
+        transition8.setDuration(Duration.seconds(6));
+        transition8.setNode(r);
+        transition8.setToY(5);
+        transition8.setAutoReverse(true);
+        transition8.setCycleCount(TranslateTransition.INDEFINITE);
+        
+        
         transition.play();
         transition1.play();
         transition2.play();
         transition3.play();
+        transition4.play();
+        transition5.play();
+        transition6.play();
+        transition7.play();
+        transition8.play();
         
     }   
     
